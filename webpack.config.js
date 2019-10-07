ebe30fe57b2b883f92563cc11347a746a52dbbb4
andrew <goluschkin.a@yandex.by>
2019-10-07 21:09:08 +0300

    modified webpack, install file-loader, add header

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,33 @@ const conf = {
                         loader:"sass-loader"
                     },
                 ]
-            }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: '[name].[ext]',
+                        publicPath:'./images',
+                        outputPath: 'images'
+                      }
+                    }
+                ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                          name: '[name].[ext]',
+                          publicPath:'./fonts',
+                          outputPath: 'fonts'
+                        }
+                    }
+                ],
+            },
         ]
     },
 }
